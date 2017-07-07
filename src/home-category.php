7d7377a1ce9aad07f3371aaa96ecc63c06bfdebe
Jose Nunez <josenunezdiaz@gmail.com>
2017-07-07 16:10:43 +1000

    Fixed category description @ home category

--- src/home-category.php
+++ src/home-category.php
@@ -8,16 +8,26 @@
 ?>
 
 	<!-- MY Works list! -->
-	<?php $cat_id=get_option('jno_front_category_id'); ?>
-	<?php query_posts( 'cat='.$cat_id ); ?>
+	<?php 
+		$cat_id=get_option('jno_front_category_id'); 
+		query_posts( 'cat='.$cat_id ); 
+	?>
+	
 	<h1><?php single_cat_title() ?></h1>
+	
 	<div class="divider divider-hammers"></div>
-	<div class="paragraph justify text-columns-2"><?php echo category_description(); ?></div>
+	
+	<?php if (category_description()): ?>
+		<div class="paragraph justify text-columns-2"><?php echo category_description(); ?></div>
+	<?php endif; ?>
+	
 	<section id="content" class="entry-container" role="main">
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'entry',get_post_format()); ?>
 			<?php comments_template(); ?>
 		<?php endwhile; endif; ?>
+		
 		<?php wp_reset_query(); ?>
 		<?php get_template_part( 'nav', 'below' ); ?>
+		
 	</section>
